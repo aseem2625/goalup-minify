a310a8d5082f73434f46339cde6c042f939d8ed3
aseemgupta <aseemgupta2625@gmail.com>
2019-01-01 21:43:07 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # goalup-minify
-Automate compression for js, css, html and image files when build tool is not integrated in your project.
+This tool automates compression of js, css, html and image files when no build tool is integrated in your project. **(WIP, Work is recently started, so watch out the space if interested)**
+
+
+**Note:** This tool is not a bundler. It uses popular tools underneath and automates certain tasks for you to have production-ready `/public`, with no setup. In a nutshell, it simply compresses, and output files individually to `/public` directory. There are more plans related to `goalup`. However, `goalup-minify` will only improve upon the sole objective it serves. This tool is great for static websites.
+
+
+#### Installation:
+
+`npm install -g goalup-minify`
+
+#### Command for compressing:
+
+`goalup-minify` // Run in working directory
+
+
+#### Options
+
+`goalup-minify --help` to list all the options
+
+
+#### Examples
+1. Compress only the listed files. `/public` is default output directory.
+
+`goalup-minify -o hello.js index.js world.css`
+
+
+2. Creates `/dist` as output directory if doesn't exist. Compress only `js` files present in the listed path.
+
+`goalup-minify --js ./js_folder -d ./dist`
+
+3. Compress js files in the path and also, index.css file if present in working directory. It ignore all the files in excluded list if present in the aforementioned paths.
+
+`goalup-minify --js ./js_folder --css -x .js_folder/touch_me_not.js .js_folder/touch_me_not.css` 
+
+4. Watch option is generally not required while development, so support will be added when required.
+
+
+#### Further ideas:
+- Parse .html files and add file name hashing for linked .js and .css files. (--hashing as option)
+- Add babel support
+- Add support for css preprocessors and auto-prefixers.
+- Add example project for real-life use case
+- Add font-icons support
+
--- index.js
+++ index.js
@@ -8,18 +8,22 @@ program
 	.description('goalup-minify automates your work of compressing js, css, html and image files when build tool is not integrated in your project.')
 
 program
-	.option('--js <path>', 'Define src path to pick javascript files. By default index.js from working directory is uglified.')
-	.option('--css <path>', 'Define src path to pick css files. By default index.css from working directory is processed.')
+	.option('--js [path]', 'Define src path to pick javascript files. By default only index.js from working directory is uglified. If option is missing, then .js are not processed.')
+	.option('--css [path]', 'Define src path to pick css files. By default only index.css from working directory is processed. If option is missing .css files are not processed.')
 	.option('--image <path>', 'Define src path to pick image files.')
-	.option('--html <path>', 'Define src path to pick html files. By default index.html from working directory is compressed.')
-	.option('-w, --watch', 'Option to watch your files and triggers respective minifiers automatically.')
-	.option('-d, --dest <dest_path>', 'Define path to output all processed files. By default dist directory is created in working directory. Output path cannot be same as current working directory.')
+	.option('--html [path]', 'Define src path to pick html files. By default only index.html from working directory is compressed. If option is missing no .html files are not processed')
+	.option('-x --exclude <files>', 'Define files to be excluded from the process.')
+	.option('-o --only <files>', 'Only these files will be compresssed. This option is to be used independent of all above options.')
+	.option('-w, --watch', 'Option to watch your above files and trigger respective minifiers automatically.')
+	.option('-d, --dest <dest_path>', 'Define path to output all processed files. By default "/public" directory is created in working directory. Output path cannot be same as current working directory.')
 	.action(function(args) {
 		minify({
 			js: args.js,
-			css: args.css, 
-			html: args.html, 
-			img: args.image, 
+			css: args.css,
+			html: args.html,
+			img: args.image,
+			exclude: args.exclude,
+			only: args.only,
 			dest: args.dest,
 			watch: args.watch
 		});
